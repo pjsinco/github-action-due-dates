1cb5976d72307d39e8ee7a5a37705a1f967813dc
Patrick Sinco <psinco@gmail.com>
2021-01-05 12:40:28 -0600

    Add action.yml

--- index.js
+++ index.js
@@ -3,21 +3,25 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 async function run() {
-  const githubToken = core.getInput('GH_TOKEN');
-  console.log(githubToken);
+  try {
+    const githubToken = core.getInput('GH_TOKEN');
+    console.log(githubToken);
 
-  if (!githubToken) {
-    throw new Error('Missing GH_TOKEN environment variable');
+    if (!githubToken) {
+      throw new Error('Missing GH_TOKEN environment variable');
+    }
+
+    const ok = new Octokit(githubToken);
+
+    const issues = await ok.listAllOpenIssues(
+      context.repo.owner,
+      context.repo.repo
+    );
+
+    console.log('okissues', issues);
+  } catch (err) {
+    core.setFailed(err.message);
   }
-
-  const ok = new Octokit(githubToken);
-
-  const issues = await ok.listAllOpenIssues(
-    context.repo.owner,
-    context.repo.repo
-  );
-
-  console.log('okissues', issues);
 }
 
 run();
